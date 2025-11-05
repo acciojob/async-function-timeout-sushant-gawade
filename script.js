41bd1f8c78e1196450b8eccb182d61db765c3332
Sushant Gawade <78841663+sushant-gawade@users.noreply.github.com>
2025-11-05 11:52:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function wait(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function displayMessage() {
+	const text = documnet.getElementById("text").value;
+	const delay = Number(document.getElementById("delay").value);
+	const output = document.getElementById("output");
+
+output.textContent = "";
+
+if (!text || inNaN(delay) || delay < 0){
+	output.textContent = "please enter valid text and delay.";
+	return;
+}
+await wait(delay);
+output.textContect = text;
+}
+document.getElementById("btn").addEventListener("click",displayMessage);
